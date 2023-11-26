f9ce6de3121d1d9c4e9d0d5f78e6aa4c12ec2f3c
dferna40 <dferna40@gmail.com>
2023-11-27 00:49:55 +0100

    Primer commit

---
+++ src/main/java/com/dferna/devTechChronicles/DevTechChroniclesApplication.java
@@ -0,0 +1,13 @@
+package com.dferna.devTechChronicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevTechChroniclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevTechChroniclesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dferna/devTechChronicles/DevTechChroniclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dferna.devTechChronicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTechChroniclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
